430dc5148d41ad71367cbc6452348bf4851268d8
midasgo <midas602461@gmail.com>
2020-01-24 00:30:35 +0900

    dummy commit

--- src/main/java/com/example/mywork/MyWorkApplication.java
+++ src/main/java/com/example/mywork/MyWorkApplication.java
@@ -2,12 +2,19 @@ package com.example.mywork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyWorkApplication
-{
+public class MyWorkApplication extends SpringBootServletInitializer {
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) 
+	{ 
+		return application.sources(MyWorkApplication.class); 
+	}
+	
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(MyWorkApplication.class, args);
-	}
+	}	
 }
